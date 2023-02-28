d5efe58df5a32ff60701a58afabed05d9a2f8b80
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2023-02-27 23:24:52 -0300

    fix text at  first page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,9 @@ import imagem from "../assets/IMG-20230202-WA0006.jpg";
 
 const Home = () => {
   return (
-    <div className="">
+    <div className="relative">
       {/*<Header></Header>*/}
-      <h1 className="absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-5xl font-bold z-10">
+      <h1 className="absolute bottom-[10vh] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl font-bold z-10">
         Breve Novidades!
       </h1>
       <img className="w-[100vw] h-[100vh]" src={imagem} alt="" />
