43bd9d2b5095926700e2b8a6a830f7eca2064fb4
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2023-02-27 23:20:10 -0300

    create first page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,12 @@ import imagem from "../assets/IMG-20230202-WA0006.jpg";
 
 const Home = () => {
   return (
-    <div className="relative">
+    <div className="">
       {/*<Header></Header>*/}
-      <h1 className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-5xl font-bold z-10">
+      <h1 className="absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-5xl font-bold z-10">
         Breve Novidades!
       </h1>
-      <img className="w-full h-full object-cover" src={imagem} alt="" />
+      <img className="w-[100vw] h-[100vh]" src={imagem} alt="" />
     </div>
   );
 };
