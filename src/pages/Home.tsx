813b8de089624b5c11bd0f51b391f6bd72e011d1
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2023-02-28 23:45:58 -0300

    create header component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import imagem from "../assets/IMG-20230202-WA0006.jpg";
 const Home = () => {
   return (
     <div className="relative">
-      {/*<Header></Header>*/}
+      <Header></Header>
       <h1 className="absolute bottom-[10vh] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl font-bold z-10">
         Breve Novidades!
       </h1>
